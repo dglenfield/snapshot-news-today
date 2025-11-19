0528bcfa38036db7d5e98889e9bbbad6cd99e98f
Danny Glenfield <dglenfield@gmail.com>
2025-11-19 18:12:22 -0500

    Add prompt files for analyze article and initial model for deserializing the response

--- SnapshotJob/SnapshotJob.Perplexity/ArticleProvider.cs
+++ SnapshotJob/SnapshotJob.Perplexity/ArticleProvider.cs
@@ -1,5 +1,7 @@
 ﻿using SnapshotJob.Common.Logging;
 using SnapshotJob.Data.Models;
+using SnapshotJob.Perplexity.Models.AnalyzeArticle;
+using SnapshotJob.Perplexity.Models.TopStories.Response;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -10,18 +12,15 @@ public class ArticleProvider(IHttpClientFactory httpClientFactory, Logger logger
 {
     public async Task Analyze(ScrapedArticle article)
     {
-        string systemContent = @"
-You are a news editor assistant.
-Return the article summary as structured JSON with the following fields: 
-custom_headline: Should be a catchy headline to grab user attention.
-summary: A detailed and informative summary about the article.
-key_points (up to 5): Each key point should be fully explained.";
+        AnalyzeArticleResult analyzeArticleResult = new();
 
-        string userContent = $@"
-Write a detailed summary for this article and a catchy headline to grab users' attention. 
-Include up to 5 key points or interesting things about the news story. I encourage you to include quotes.
-Here is the article content: {article.Content}
-";
+        string systemPromptFileName = "analyze-article-system-prompt.txt";
+        string systemPromptFilePath = Path.Combine(AppContext.BaseDirectory, "Prompts", systemPromptFileName);
+        string systemContent = File.ReadAllText(systemPromptFilePath);
+
+        string userPromptFileName = "analyze-article-user-prompt.txt";
+        string userPromptFilePath = Path.Combine(AppContext.BaseDirectory, "Prompts", userPromptFileName);
+        string userContent = $"{File.ReadAllText(userPromptFilePath)}{TrimInnerHtmlWhitespace(article.Content ?? string.Empty)}";
 
         var requestBody = new
         {
@@ -72,7 +71,7 @@ Here is the article content: {article.Content}
 
         //logger.Log(await jsonContent.ReadAsStringAsync());
 
-        string file = "C:\\Users\\danny\\OneDrive\\Projects\\SnapshotNewsToday\\TestData\\top-stories-response_2025-11-18.json";
+        string file = "C:\\Users\\danny\\OneDrive\\Projects\\SnapshotNewsToday\\TestData\\analyze-article-response_2025-11-19.json";
         var responseString = await File.ReadAllTextAsync(file);
 
         //var response = await httpClientFactory.CreateClient("Perplexity").PostAsync("", jsonContent);
@@ -83,8 +82,19 @@ Here is the article content: {article.Content}
         //}
 
         //var responseString = await response.Content.ReadAsStringAsync();
-        logger.Log(responseString);
+        //logger.Log("\n" + responseString);
         //return Content(responseString, "application/json");
+
+        var apiResponse = JsonSerializer.Deserialize<Response>(responseString);
+        var contentJson = apiResponse?.Choices[0].Message.Content;
+        
+        // Remove opening and closing brackets
+        contentJson = (contentJson?.Length > 2) ? contentJson.Substring(1, contentJson.Length - 2) : string.Empty;
+
+        var analyzeArticleResponse = JsonSerializer.Deserialize<AnalyzeArticleContent>(contentJson);
+        logger.Log("\n" + analyzeArticleResponse);
+
+        analyzeArticleResult.PerplexityApiUsage = apiResponse.Usage;
     }
 
     private string TrimInnerHtmlWhitespace(string html)
---
+++ SnapshotJob/SnapshotJob.Perplexity/Models/AnalyzeArticle/AnalyzeArticleResult.cs
@@ -0,0 +1,22 @@
+﻿using SnapshotJob.Common.Serialization;
+using SnapshotJob.Perplexity.Models.TopStories.Response;
+using System.Text.Json;
+
+namespace SnapshotJob.Perplexity.Models.AnalyzeArticle;
+
+public class AnalyzeArticleResult
+{
+    public string? RequestBody { get; set; }
+    public string? ResponseString { get; set; }
+    public Exception? Exception { get; set; }
+
+    // PerplexityResponse
+    public string PerplexityResponseId { get; set; } = default!;
+    public string PerplexityResponseModel { get; set; } = default!;
+    public Usage PerplexityApiUsage { get; set; } = default!;
+    public List<string> Citations { get; set; } = default!;
+    public List<SearchResult> SearchResults { get; set; } = default!;
+
+    public override string ToString() => JsonConfig.ToJson(this, JsonSerializerOptions.Default,
+        CustomJsonSerializerOptions.IgnoreNull | CustomJsonSerializerOptions.WriteIndented);
+}
