9f73f57000cb90e2858a39374efbb8a489fac6c8
Danny Glenfield <dglenfield@gmail.com>
2025-11-23 12:13:09 -0500

    Add SnapshotNewsToday.Data project for Azure CosmosDB functionality
    - Create models folder
    - Create ApplicationDbContext

---
+++ SnapshotNewsToday.Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SnapshotNewsToday.Data.Models;
+
+namespace SnapshotNewsToday.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Article> Articles => Set<Article>();
+
+    public ApplicationDbContext() : base()
+    {
+
+    }
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Article>().ToContainer("Articles");
+    }
+}
